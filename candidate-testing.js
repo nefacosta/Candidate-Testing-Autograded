7794d66181f944cd9c5360b85ceddaadf364a553
Nef <nefacosta1@gmail.com>
2023-09-28 23:56:06 -0500

    modified:   candidate-testing.js commit

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,9 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", 3];
+let candidateAnswers = [""]; //No defining makes output undefined, = []; just has correct answers no user's answers, = ""; just has correct answers no user's answers, = [""]; just has correct answers no user's answers
 
 
 function askForName() {
@@ -24,23 +24,32 @@ console.log(candidateName);
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
- 
-  candidateAnswer = input.question(question);
-  // return candidateAnswer;
-  console.log(candidateAnswer);   
-} 
+  for (let i = 0; i < questions.length; i++) {
+    console.log(candidateAnswers = input.question(questions[i]));
 
-// console.log(candidateAnswer);
+//return (candidateAnswers = input.question(questions[i])); This line of code asks the first question twice and comes out with one answer
+//return (candidateAnswers = console.log(input.question(questions[i]))); This line of code asks the first question twice & prints the user's answer and comes out with an undefined
+//console.log(questions[i]);
+//candidateAnswers = input.question(questions);
+//console.log(candidateAnswers); This line of code proves that candidateAnswers was changed when the user answered but it doesn't apply to outside the function
+} 
+//return candidateAnswers;
+//console.log(candidateAnswers); While this console.log only prints out the very last answer and not every answer that was inputted
+}
+//console.log(candidateAnswers); This doesn't print because it's outside the function
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-    if (candidateAnswer == correctAnswer) {
-    (console.log(true));
-  
-}   else {
-  (console.log(false));
-}
+  console.log(`${candidateAnswers} ${correctAnswers}`);
+
+/*
+console.log(candidateAnswers= askQuestion() + correctAnswers); This line of code reasks the questions but doesn't state the users answers alongside the correct answers. Try something else.
+console.log(`${askQuestion()} ${correctAnswers}`); This line of code has the same issue. It keeps asking the question again
+console.log(`${candidateAnswers = askQuestion()} ${correctAnswers}`); This line of code Doesn't list user's answers and only has undefined alongside correct answers
+console.log(`${candidateAnswers[]} ${correctAnswers}`); Has no users answers and only the correct answers
+console.log(`${candidateAnswers[0][1]} ${correctAnswers}`); [1] is undefined with just [0] by itself then it's still undefined
+*/
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
